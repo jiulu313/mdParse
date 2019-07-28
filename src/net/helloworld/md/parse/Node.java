1122e425b46dd8bf806375cf8b9cd47067c912f4
张红军 <zhanghongjun228@163.com>
2019-07-28 22:43:53 +0800

    添加demo

--- src/net/helloworld/md/parse/Node.java
+++ src/net/helloworld/md/parse/Node.java
@@ -1,5 +1,7 @@
 package net.helloworld.md.parse;
 
+import java.util.List;
+
 public abstract class Node {
     //节点类型
     protected int type;
@@ -7,6 +9,45 @@ public abstract class Node {
     protected String tag;
     //作用域，单行与多行
     protected int scope;
+    //子Node
+    protected List<Node> children;
+
+
+    public Node(int type){
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public int getScope() {
+        return scope;
+    }
+
+    public void setScope(int scope) {
+        this.scope = scope;
+    }
+
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Node> children) {
+        this.children = children;
+    }
 
 
 
