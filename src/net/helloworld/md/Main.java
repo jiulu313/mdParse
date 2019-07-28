5cd0b062d299731546bff7b2c9c2fcb769909393
张红军 <zhanghongjun228@163.com>
2019-07-28 22:24:18 +0800

    添加项目

---
+++ src/net/helloworld/md/Main.java
@@ -0,0 +1,12 @@
+package net.helloworld.md;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello,world");
+
+    }
+}
+
+
---
+++ src/net/helloworld/md/parse/Node.java
@@ -0,0 +1,14 @@
+package net.helloworld.md.parse;
+
+public abstract class Node {
+    //节点类型
+    protected int type;
+    //标签
+    protected String tag;
+    //作用域，单行与多行
+    protected int scope;
+
+
+
+
+}
